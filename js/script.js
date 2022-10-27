c507b0f3c8b67bc1dbe913bf83eeccc3e5391d24
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-27 15:36:45 +0200

    base progetto, aggiungere done

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Descrizione:
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+//MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+//MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+//MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            hasError: false,
+            lineTrough: false,
+            newTask: {
+                name: "",
+                done: false,
+            },
+            tasks: [
+                {
+                    name: "fare la Spesa",
+                    done: false,
+                },
+                {
+                    name: "pulire il bagno",
+                    done: false,
+                },
+                {
+                    name: "guardare anime",
+                    done: false,
+                },
+
+            ],
+        }
+    },
+    methods: {
+        addNewTask() {
+
+            //inserire task solo se ha più di 5 caratteri
+            if (this.newTask.name.length < 5) {
+                this.hasError = true;
+            } else {
+                //.unshift permette di aggiungere un elemento all'inizio
+                this.tasks.unshift({ ...this.newTask });
+                this.newTask.name = "";
+                this.hasError = false;
+            }
+        },
+        deleteTask(indexOfTasks) {
+            //quando terminiamo tutti i task facciamo apparire una scritta
+            this.tasks.splice(indexOfTasks, 1);
+        },
+    }
+}).mount("#app")
